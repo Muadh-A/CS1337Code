69d7320fe7a38dc74a3cd8bd711b8f9e29efb76b
Muadh Asdi <muadhaasdi@gmail.com>
2026-02-09 12:09:39 -0600

    add returnBasics.cpp

---
+++ Basics/returnBasics.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+}
+
+
+// Goal: Understand that a function returns a value and main uses it.
+
+// Task:
+
+// Write a function called add that:
+
+// Takes two ints
+
+// Returns their sum
+
+// In main:
+
+// Call add(3, 5)
+
+// Store the result in a variable
+
+// Print the result
+
+// No loops. No arrays. No tricks.
